da88b72ba6cd55d51af544300f85e51153f1e8b1
serbiow <sergiohenriqueqqq@gmail.com>
2024-02-23 10:31:06 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var adicao = require('./adicao')
+var subtracao = require('./subtracao')
+var multiplicacao = require('./multiplicacao')
+var divisao = require('./divisao')
+
+console.log("Adição: " + adicao(20,5))
+console.log("Subtração: " + subtracao(20,5))
+console.log("Multiplicação: " + multiplicacao(20,5))
+console.log("Divisão: " + divisao(20,5))
